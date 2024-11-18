83cbbf52f1ed941bdd85a998eed730e0415fa00f
kaye-sub <kwonk0214@gmail.com>
2024-11-18 18:40:47 +0900

    song-list: � 노래 출력

--- react-song-list/src/App.js
+++ react-song-list/src/App.js
@@ -4,12 +4,29 @@ function App() {
   return (
     <div>
       <Header />
+      <Playlist />
+      <Container />
     </div>
   );
 }
 
-function Header() { 
+function Header() {
   return <h1>React 프로그래밍</h1>;
+}
+
+function Playlist() {
+  return <div className="playlist">프로그래밍하면서 듣고 싶은 노래</div>;
+}
+
+function Container() {
+  return (
+    <div className="container">
+      <a href="https://www.youtube.com/results?search_query=숲">
+        <img src="https://picsum.photos/600/150?random=1" alt="랜덤 이미지 1" />
+        <div className="song-title">숲</div>
+      </a>
+    </div>
+  );
 }
 
 export default App;
